faebb51e407cba7a72e159080af2e6bcfe7265bd
Andrew <ajergy@gmail.com>
2024-04-09 22:04:29 -0600

    Fix a couple details.

--- javascript/EncounterCreatorMain.js
+++ javascript/EncounterCreatorMain.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (socket.readyState === WebSocket.OPEN) {
             socket.send(updateMessage);
         }
-    }, getRandomTime(1, 2) * 1000); // Convert seconds to milliseconds
+    }, getRandomTime(10, 15) * 1000); // Convert seconds to milliseconds
 });
 
 // Function to generate a random time interval between min and max values (inclusive)
@@ -111,15 +111,118 @@ const partiesData = {
         { name: "Grimnir", race: "Human", level: 1, class: "Druid" },
         { name: "Lyra", race: "Human", level: 1, class: "Brawler" },
         { name: "Sorin", race: "Human", level: 1, class: "Fighter" }
+    ],
+    "Glugnog 2": [
+        { name: "Spectrum", race: "Fire Genasi", level: 7, class: "Cleric" },
+        { name: "Gnorbert", race: "Gnome", level: 7, class: "Sorcerer" },
+        { name: "Barystios", race: "Centaur", level: 7, class: "Barbarian" },
+        { name: "Kia", race: "Wood Elf", level: 7, class: "Monk" }
+    ],
+    "Kaazime and Pals 2": [
+        { name: "Kaazime", race: "Air Genasi", level: 10, class: "Warlock" },
+        { name: "Jandar, the Forgotten", race: "High Elf", level: 10, class: "Ranger" },
+        { name: "Leika", race: "Human", level: 10, class: "Fighter" },
+        { name: "Cassius", race: "Half-elf", level: 10, class: "Cleric" },
+        { name: "Feld", race: "Dwarf", level: 10, class: "Paladin" },
+        { name: "Nova", race: "Human", level: 10, class: "Rogue" }
+    ],
+    "Biscuits and Kwazy 2": [
+        { name: "Branwyn", race: "Human", level: 3, class: "Paladin" },
+        { name: "Teddy", race: "Tabaxi", level: 3, class: "Fighter" },
+        { name: "Mabel", race: "Wood Elf", level: 3, class: "Cleric" },
+        { name: "Luna", race: "Warforged", level: 3, class: "Wizard" }
+    ],
+    "Vox Machina 2": [
+        { name: "Vex", race: "Half-elf", level: 13, class: "Multiclass" },
+        { name: "Vax", race: "Half-elf", level: 13, class: "Ranger" },
+        { name: "Scanlan", race: "Gnome", level: 13, class: "Bard" },
+        { name: "Grog", race: "Goliath", level: 13, class: "Barbarian" },
+        { name: "Pike", race: "Gnome", level: 13, class: "Cleric" },
+        { name: "Percy", race: "Human", level: 13, class: "Fighter" },
+        { name: "Keyleth", race: "Half-elf", level: 13, class: "Druid" }
+    ],
+    "The Mighty Nein 2": [
+        { name: "Caleb", race: "Human", level: 9, class: "Wizard" },
+        { name: "Jester", race: "Tiefling", level: 9, class: "Cleric" },
+        { name: "Fjord", race: "Half-orc", level: 9, class: "Warlock" },
+        { name: "Nott", race: "Goblin", level: 9, class: "Rogue" },
+        { name: "Caduceus", race: "Firbolg", level: 9, class: "Cleric" },
+        { name: "Beauregard", race: "Human", level: 9, class: "Monk" },
+        { name: "Yasha", race: "Assimar", level: 9, class: "Barbarian" }
+    ],
+    "Bells Hells 2": [
+        { name: "Ashton", race: "Human", level: 4, class: "Barbarian" },
+        { name: "FCG", race: "Warforged", level: 4, class: "Cleric" },
+        { name: "Chetney", race: "Human", level: 4, class: "Rogue" },
+        { name: "Dorian", race: "Human", level: 4, class: "Bard" },
+        { name: "Fearne", race: "Human", level: 4, class: "Druid" },
+        { name: "Imogen", race: "Human", level: 4, class: "Sorcerer" },
+        { name: "Laudna", race: "Human", level: 4, class: "Warlock" }
+    ],
+    "Whimsical Wanderers 2": [
+        { name: "Ariadne", race: "Human", level: 1, class: "Paladin" },
+        { name: "Thalian", race: "Human", level: 1, class: "Warlock" },
+        { name: "Grimnir", race: "Human", level: 1, class: "Druid" },
+        { name: "Lyra", race: "Human", level: 1, class: "Brawler" },
+        { name: "Sorin", race: "Human", level: 1, class: "Fighter" }
+    ],
+    "Glugnog 3": [
+        { name: "Spectrum", race: "Fire Genasi", level: 7, class: "Cleric" },
+        { name: "Gnorbert", race: "Gnome", level: 7, class: "Sorcerer" },
+        { name: "Barystios", race: "Centaur", level: 7, class: "Barbarian" },
+        { name: "Kia", race: "Wood Elf", level: 7, class: "Monk" }
+    ],
+    "Kaazime and Pals 3": [
+        { name: "Kaazime", race: "Air Genasi", level: 10, class: "Warlock" },
+        { name: "Jandar, the Forgotten", race: "High Elf", level: 10, class: "Ranger" },
+        { name: "Leika", race: "Human", level: 10, class: "Fighter" },
+        { name: "Cassius", race: "Half-elf", level: 10, class: "Cleric" },
+        { name: "Feld", race: "Dwarf", level: 10, class: "Paladin" },
+        { name: "Nova", race: "Human", level: 10, class: "Rogue" }
+    ],
+    "Biscuits and Kwazy 3": [
+        { name: "Branwyn", race: "Human", level: 3, class: "Paladin" },
+        { name: "Teddy", race: "Tabaxi", level: 3, class: "Fighter" },
+        { name: "Mabel", race: "Wood Elf", level: 3, class: "Cleric" },
+        { name: "Luna", race: "Warforged", level: 3, class: "Wizard" }
+    ],
+    "Vox Machina 3": [
+        { name: "Vex", race: "Half-elf", level: 13, class: "Multiclass" },
+        { name: "Vax", race: "Half-elf", level: 13, class: "Ranger" },
+        { name: "Scanlan", race: "Gnome", level: 13, class: "Bard" },
+        { name: "Grog", race: "Goliath", level: 13, class: "Barbarian" },
+        { name: "Pike", race: "Gnome", level: 13, class: "Cleric" },
+        { name: "Percy", race: "Human", level: 13, class: "Fighter" },
+        { name: "Keyleth", race: "Half-elf", level: 13, class: "Druid" }
+    ],
+    "The Mighty Nein 3": [
+        { name: "Caleb", race: "Human", level: 9, class: "Wizard" },
+        { name: "Jester", race: "Tiefling", level: 9, class: "Cleric" },
+        { name: "Fjord", race: "Half-orc", level: 9, class: "Warlock" },
+        { name: "Nott", race: "Goblin", level: 9, class: "Rogue" },
+        { name: "Caduceus", race: "Firbolg", level: 9, class: "Cleric" },
+        { name: "Beauregard", race: "Human", level: 9, class: "Monk" },
+        { name: "Yasha", race: "Assimar", level: 9, class: "Barbarian" }
+    ],
+    "Bells Hells 3": [
+        { name: "Ashton", race: "Human", level: 4, class: "Barbarian" },
+        { name: "FCG", race: "Warforged", level: 4, class: "Cleric" },
+        { name: "Chetney", race: "Human", level: 4, class: "Rogue" },
+        { name: "Dorian", race: "Human", level: 4, class: "Bard" },
+        { name: "Fearne", race: "Human", level: 4, class: "Druid" },
+        { name: "Imogen", race: "Human", level: 4, class: "Sorcerer" },
+        { name: "Laudna", race: "Human", level: 4, class: "Warlock" }
+    ],
+    "Whimsical Wanderers ": [
+        { name: "Ariadne", race: "Human", level: 1, class: "Paladin" },
+        { name: "Thalian", race: "Human", level: 1, class: "Warlock" },
+        { name: "Grimnir", race: "Human", level: 1, class: "Druid" },
+        { name: "Lyra", race: "Human", level: 1, class: "Brawler" },
+        { name: "Sorin", race: "Human", level: 1, class: "Fighter" }
     ]
 };
 
 // Define encounter data in a single object
-const encountersDataOld = {
-    "Mummies": ["Mummy"],
-    "Water Crater": ["Water Elemental", "Earth Elemental", "Mud Elemental"]
-};
-
 const encountersData = {
     "Troll Surprise": [
         { name: "Troll", quantity: 1, AC: 15, HP: 84, attackBonus: 7, saveDC: null, avgDamage: 28 },
@@ -140,6 +243,52 @@ const encountersData = {
         { name: "Mummy", quantity: 6, AC: 11, HP: 58, attackBonus: 5, saveDC: 12, avgDamage: 20 }
     ],
     "Water Crater": [
+        { name: "Water Elemental", quantity: 2, AC: 14, HP: 114, attackBonus: 7, saveDC: 15, avgDamage: 26 },
+        { name: "Earth Elemental", quantity: 2, AC: 17, HP: 126, attackBonus: 8, saveDC: null, avgDamage: 28 },
+        { name: "Mud Elemental", quantity: 8, AC: 14, HP: 61, attackBonus: 6, saveDC: null, avgDamage: 12 }
+    ],
+    "Troll Surprise 2": [
+        { name: "Troll", quantity: 1, AC: 15, HP: 84, attackBonus: 7, saveDC: null, avgDamage: 28 },
+        { name: "Goblin Boss", quantity: 1, AC: 17, HP: 21, attackBonus: 4, saveDC: null, avgDamage: 8 },
+        { name: "Goblin", quantity: 8, AC: 15, HP: 7, attackBonus: 4, saveDC: null, avgDamage: 5 }
+    ],
+    "Yiga Ambush 2": [
+        { name: "Yiga Footsoldier", quantity: 3, AC: 14, HP: 52, attackBonus: 5, saveDC: 13, avgDamage: 14 },
+        { name: "Viga Archer", quantity: 3, AC: 14, HP: 52, attackBonus: 5, saveDC: 11, avgDamage: 14 }
+    ],
+    "Kohga 2": [
+        { name: "Master Kohga", quantity: 1, AC: 18, HP: 127, attackBonus: 9, saveDC: 17, avgDamage: 12 },
+        { name: "Yiga Footsoldier", quantity: 3, AC: 14, HP: 52, attackBonus: 5, saveDC: 13, avgDamage: 14 },
+        { name: "Viga Archer", quantity: 3, AC: 14, HP: 52, attackBonus: 5, saveDC: 11, avgDamage: 14 },
+        { name: "Viga Blademaster", quantity: 1, AC: 16, HP: 84, attackBonus: 7, saveDC: 15, avgDamage: 20 }
+    ],
+    "Mummies 2": [
+        { name: "Mummy", quantity: 6, AC: 11, HP: 58, attackBonus: 5, saveDC: 12, avgDamage: 20 }
+    ],
+    "Water Crater 2": [
+        { name: "Water Elemental", quantity: 2, AC: 14, HP: 114, attackBonus: 7, saveDC: 15, avgDamage: 26 },
+        { name: "Earth Elemental", quantity: 2, AC: 17, HP: 126, attackBonus: 8, saveDC: null, avgDamage: 28 },
+        { name: "Mud Elemental", quantity: 8, AC: 14, HP: 61, attackBonus: 6, saveDC: null, avgDamage: 12 }
+    ],
+    "Troll Surprise 3": [
+        { name: "Troll", quantity: 1, AC: 15, HP: 84, attackBonus: 7, saveDC: null, avgDamage: 28 },
+        { name: "Goblin Boss", quantity: 1, AC: 17, HP: 21, attackBonus: 4, saveDC: null, avgDamage: 8 },
+        { name: "Goblin", quantity: 8, AC: 15, HP: 7, attackBonus: 4, saveDC: null, avgDamage: 5 }
+    ],
+    "Yiga Ambush 3": [
+        { name: "Yiga Footsoldier", quantity: 3, AC: 14, HP: 52, attackBonus: 5, saveDC: 13, avgDamage: 14 },
+        { name: "Viga Archer", quantity: 3, AC: 14, HP: 52, attackBonus: 5, saveDC: 11, avgDamage: 14 }
+    ],
+    "Kohga 3": [
+        { name: "Master Kohga", quantity: 1, AC: 18, HP: 127, attackBonus: 9, saveDC: 17, avgDamage: 12 },
+        { name: "Yiga Footsoldier", quantity: 3, AC: 14, HP: 52, attackBonus: 5, saveDC: 13, avgDamage: 14 },
+        { name: "Viga Archer", quantity: 3, AC: 14, HP: 52, attackBonus: 5, saveDC: 11, avgDamage: 14 },
+        { name: "Viga Blademaster", quantity: 1, AC: 16, HP: 84, attackBonus: 7, saveDC: 15, avgDamage: 20 }
+    ],
+    "Mummies 3": [
+        { name: "Mummy", quantity: 6, AC: 11, HP: 58, attackBonus: 5, saveDC: 12, avgDamage: 20 }
+    ],
+    "Water Crater 3": [
         { name: "Water Elemental", quantity: 2, AC: 14, HP: 114, attackBonus: 7, saveDC: 15, avgDamage: 26 },
         { name: "Earth Elemental", quantity: 2, AC: 17, HP: 126, attackBonus: 8, saveDC: null, avgDamage: 28 },
         { name: "Mud Elemental", quantity: 8, AC: 14, HP: 61, attackBonus: 6, saveDC: null, avgDamage: 12 }
