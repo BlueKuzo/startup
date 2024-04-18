fd647828980c66fb8073dea4e2131f3905d29382
Andrew <ajergy@gmail.com>
2024-04-18 00:47:39 -0600

    Clean up login page and add Register button.

--- public/login.js
+++ public/login.js
@@ -1,17 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
     const loginButton = document.getElementById("login");
+    const registerButton = document.getElementById("register");
 
     loginButton.addEventListener("click", function() {
+        const emailInput = document.getElementById("email").value;
         const usernameInput = document.getElementById("username").value;
         const passwordInput = document.getElementById("password").value;
 
-        if (usernameInput.trim() === "" && passwordInput.trim() === "") {
-            alert("Missing username and password!");
-        } else if (usernameInput.trim() === "") {
-            alert("Missing username!");
-        } else if (passwordInput.trim() === "") {
+        if (emailInput.trim() === "" && usernameInput.trim() === "" && passwordInput.trim() === "") {
+            alert("Please enter either your email address or username, and password!");
+        }
+        
+        else if ((emailInput.trim() === "" && usernameInput.trim() === "")) {
+            alert("Missing email/username!");
+        }
+        
+        else if (passwordInput.trim() === "") {
             alert("Missing password!");
-        } else {
+        }
+        
+        else {
+            // Navigate to EncounterCreatorMain.html
+            window.location.href = "EncounterCreatorMain.html";
+        }
+    });
+
+    registerButton.addEventListener("click", function() {
+        const emailInput = document.getElementById("email").value;
+        const usernameInput = document.getElementById("username").value;
+        const passwordInput = document.getElementById("password").value;
+
+        if (emailInput.trim() === "" || usernameInput.trim() === "" || passwordInput.trim() === "") {
+            alert("Please enter either your email address, a username, and a password!");
+        }
+        
+        else {
             // Navigate to EncounterCreatorMain.html
             window.location.href = "EncounterCreatorMain.html";
         }
